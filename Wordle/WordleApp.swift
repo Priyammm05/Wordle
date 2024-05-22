f3d6f48bf654756af3c0daa5c564ec6c24c77360
Priyammm05 <priyam05soni@gmail.com>
2024-05-22 16:09:32 +0530

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Priyam Soni on 22/05/24.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
