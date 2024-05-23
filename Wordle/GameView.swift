08a4fa8d930256e7b3674fc63e9e9f3c91bba6ce
Priyammm05 <priyam05soni@gmail.com>
2024-05-23 13:38:44 +0530

    Created the Game view and the start view UI for the Wordle

---
+++ Wordle/GameView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  Wordle
+//
+//  Created by Priyam Soni on 22/05/24.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    @EnvironmentObject var viewModel: WordleDataModel
+    
+    var body: some View {
+        NavigationView{
+            VStack(spacing: 3){
+                ForEach(0...5, id: \.self){ index in
+                    GuessView(guess: $viewModel.guesses[index])
+                }
+            }
+            .frame(width: Global.boardWidth, height: 6 * Global.boardWidth / 5)
+            .padding()
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar{
+                ToolbarItem(placement: .topBarLeading) {
+                    Button{
+                        
+                    }label: {
+                        Image(systemName: "questionmark.circle")
+                    }
+                }
+                    
+                ToolbarItem(placement: .principal) {
+                    Text("WORDLE")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(.primary)
+                }
+                    
+                ToolbarItem(placement: .topBarTrailing) {
+                    HStack{
+                        Button{
+                                
+                        }label: {
+                            Image(systemName: "chart.bar")
+                        }
+                            
+                        Button{
+                            
+                        }label: {
+                            Image(systemName: "gearshape.fill")
+                        }
+                    }
+                }
+            }
+        }
+        .navigationViewStyle(.stack)
+    }
+}
+
+#Preview {
+    GameView()
+        .environmentObject(WordleDataModel())
+}
