e29fb1d9e6a7bdf0c51ed9348b7f1e7254b09e16
Priyammm05 <priyam05soni@gmail.com>
2024-05-23 13:36:07 +0530

    Created color extension for the system background

---
+++ Wordle/Extensions/ColorExtension.swift
@@ -0,0 +1,13 @@
+//
+//  ColorExtension.swift
+//  Wordle
+//
+//  Created by Priyam Soni on 23/05/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let systemBackground = Color(UIColor.systemBackground)
+}
